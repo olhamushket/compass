a6c3d7ad83315de456e95fb8242ac4ccacfc226a
Olha Mushket <olhamushket@gmail.com>
2022-07-15 01:00:08 +0300

    Solved

--- src/main/java/com/epam/rd/autotasks/Direction.java
+++ src/main/java/com/epam/rd/autotasks/Direction.java
@@ -11,19 +11,54 @@ public enum Direction {
 
     private int degrees;
 
+    private static int inRange (int degrees) {
+        while (degrees < 0) degrees += 360;
+        return degrees % 360;
+    }
+
     public static Direction ofDegrees(int degrees) {
-        throw new UnsupportedOperationException();
+        degrees = inRange(degrees);
+        for (Direction dir : Direction.values()) {
+            if (dir.degrees == degrees) {
+                return dir;
+            }
+        }
+        return null;
+//        throw new UnsupportedOperationException();
     }
 
     public static Direction closestToDegrees(int degrees) {
-        throw new UnsupportedOperationException();
+        degrees = inRange(degrees);
+        if (degrees < 23) return N;
+        if (degrees < 67) return NE;
+        if (degrees < 112) return E;
+        if (degrees < 157) return  SE;
+        if (degrees < 202) return S;
+        if (degrees < 247) return SW;
+        if (degrees < 292) return W;
+        if (degrees < 337) return NW;
+        return N;
+//        throw new UnsupportedOperationException();
     }
 
     public Direction opposite() {
-        throw new UnsupportedOperationException();
+        switch (this) {
+            case N: return S;
+            case NE: return SW;
+            case E: return W;
+            case SE: return NW;
+            case S: return N;
+            case SW: return NE;
+            case W: return E;
+            case NW: return SE;
+        }
+        return null;
+//        throw new UnsupportedOperationException();
     }
 
     public int differenceDegreesTo(Direction direction) {
-        throw new UnsupportedOperationException();
+        int difference = Math.abs(this.degrees - direction.degrees);
+        return Math.min(difference, 360 - difference);
+//        throw new UnsupportedOperationException();
     }
 }
